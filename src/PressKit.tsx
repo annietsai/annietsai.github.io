0a3e1e80ea26183217ccb7246ce0603014834b64
annietsai <tsaiannie1@gmail.com>
2025-12-30 01:11:24 -0800

    Make footer stick to bottom

--- src/PressKit.tsx
+++ src/PressKit.tsx
@@ -1,5 +1,6 @@
 import Nav from './Nav';
 import Footer from './Footer';
+import { motion } from 'framer-motion';
 
 const PressKit: React.FC = () => {
   return (
@@ -8,7 +9,14 @@ const PressKit: React.FC = () => {
 
       <main className="press-kit-content">
         <h1>ELECTRONIC PRESS KIT</h1>
-        TODO
+        <motion.div
+          className="cta"
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, ease: 'easeOut', delay: 0.4 }}
+        >
+          COMING SOON
+        </motion.div>
       </main>
 
       <Footer />
